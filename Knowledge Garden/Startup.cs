c393d7f04765413d13728a90b6bace707acc65ad
m.shraky <m.shraky@civil1000.civilsoft.net>
2019-07-22 14:21:47 +0200

    Initial commit

---
+++ Knowledge Garden/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Knowledge_Garden.Startup))]
+namespace Knowledge_Garden
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
