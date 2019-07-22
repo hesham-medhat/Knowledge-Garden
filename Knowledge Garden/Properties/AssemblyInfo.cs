19d73380caad2a9343a703fa9ace0dacad1abd36
m.shraky <m.shraky@civil1000.civilsoft.net>
2019-07-22 15:19:44 +0200

    Personalize About and Contact adding ApplicationInformation class to hold info in constant strings

--- Knowledge Garden/Properties/AssemblyInfo.cs
+++ Knowledge Garden/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Knowledge_Garden")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Knowledge sharing web application where employees share their experience facing and dealing with technical issues")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("CivilSoft")]
 [assembly: AssemblyProduct("Knowledge_Garden")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
