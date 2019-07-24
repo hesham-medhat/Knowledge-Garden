16750da817b12e11d12f197aa7457fcc11f6abdc
hesham-medhat <heshammedhat5@gmail.com>
2019-07-24 15:06:05 +0200

    Add SignalR to project

---
+++ Knowledge Garden.Engine/Models/ApplicationInformation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Knowledge_Garden.Models
+{
+    /// <summary>
+    /// The class responsible for holding general information of the application in the
+    /// intent of having less hard-coded strings in views
+    /// </summary>
+    public static class ApplicationInformation
+    {
+        public const string ShortDescription = "Knowledge sharing web application where employees share their experience facing and dealing with technical issues.";
+
+        public const string Credits = "Created by Hesham Medhat as part of the summer internship in summer 2019 at CivilSoft company's branch in Alexandria, Egypt.";
+
+    }
+}
---
+++ Knowledge Garden.Engine/SignalR/NotificationsHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Knowledge_Garden.Engine.SignalR
+{
+    [HubName("notifications")]
+    class NotificationsHub : Hub
+    {
+        public IEnumerable<string> GetUnreadNotifications()
+        {
+            throw new NotImplementedException("GetUnreadNotifications is not implemented");
+        }
+    }
+}
