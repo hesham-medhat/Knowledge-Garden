ef5a5ffe84a45da79ef6a7ab7e57b3d2e482fb5d
hesham-medhat <heshammedhat5@gmail.com>
2019-07-31 16:00:38 +0200

    Implement server-side of fetching notifications and connect to signalR

--- Knowledge Garden.Engine/SignalR/NotificationsHub.cs
+++ Knowledge Garden.Engine/SignalR/NotificationsHub.cs
@@ -5,15 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
+using Knowledge_Garden.Engine.DataAccess;
+using Knowledge_Garden.Engine.DataAccess.Repositories;
 
 namespace Knowledge_Garden.Engine.SignalR
 {
     [HubName("notifications")]
     class NotificationsHub : Hub
     {
-        public IEnumerable<string> GetUnreadNotifications()
+        public IEnumerable<KeyValuePair<int, string>> GetUnreadNotifications()
         {
-            throw new NotImplementedException("GetUnreadNotifications is not implemented");
+            string username = Context.User.Identity.Name;
+            
+            IUnitOfWork uow = new UnitOfWork();
+
+            var returner = uow.Notifications.GetNotifications(username);
+
+            uow.SaveAndDispose();
+
+            return returner;
         }
     }
 }
