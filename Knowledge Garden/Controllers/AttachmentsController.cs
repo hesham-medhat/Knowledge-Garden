3c2952554d5037412ef1fc697ce88f73676da40c
hesham-medhat <heshammedhat5@gmail.com>
2019-08-08 12:42:24 +0200

    Implement all manipulation for attachments

--- Knowledge Garden/Controllers/AttachmentsController.cs
+++ Knowledge Garden/Controllers/AttachmentsController.cs
@@ -18,9 +18,89 @@ namespace Knowledge_Garden.Controllers
         private IUnitOfWork uow = new UnitOfWork();
 
 
-        [Route("Attachments/Details/{flowerId?}/{attachmentName}")]
-        public ActionResult Details(int flowerId, string attachmentName)
+        public ActionResult Create(int? id)
         {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Flower id not specified");
+            }
+            ViewBag.flowerId = id.Value;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(int flowerId, HttpPostedFileBase fileBase)
+        {
+            // Get requested flower
+            Flower flower = uow.Flowers.Find(flowerId);
+            if (flower == null)
+            {
+                return HttpNotFound("Flower doesn't exist");
+            }
+
+            // Confirm user owns that flower
+            if (flower.OwnerUsername != User.Identity.Name)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Forbidden, "You cannot add an attachment to a flower you don't own");
+            }
+
+            // Determine if legal file type
+            switch (fileBase.ContentType)
+            {
+                case "application/vnd.microsoft.portable-executable":
+                case "application/octet-stream":
+                case "application/x-msdownload":
+                    return new HttpStatusCodeResult(HttpStatusCode.Forbidden, "Cannot upload executables");
+                default: break;
+            }
+
+            // Build attachment
+            Attachment newAttachment = new Attachment
+            {
+                ContentType = fileBase.ContentType,
+                Name = fileBase.FileName,
+                Flower = flower
+            };
+            newAttachment.blobValue = new byte[fileBase.ContentLength];
+            fileBase.InputStream.Read(newAttachment.blobValue, 0, (fileBase.ContentLength));
+
+            uow.Attachments.Add(newAttachment);
+
+            // All good. Terminate.
+            return RedirectToAction("Index", new { id = flowerId });
+        }
+        
+        public ActionResult Index(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Not given an ID for the flower whose attachments are requested");
+            }
+            // Get requested flower
+            Flower flower = uow.Flowers.Find(id);
+            if (flower == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (flower.OwnerUsername != User.Identity.Name)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Forbidden, "You do not own the flower in question");
+            }
+
+            // Build view
+            ViewBag.flowerId = id;
+            return View(flower.Attachments);
+        }
+
+        [Route("Attachments/Details/{flowerId?}/{attachmentName}")]
+        public ActionResult Details(int? flowerId, string attachmentName)
+        {
+            if (flowerId == null || attachmentName == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Missing parameters for the request.");
+            }
             Attachment attachment = uow.Attachments.Find(flowerId, attachmentName);
             if (attachment == null)
             {
@@ -32,10 +112,14 @@ namespace Knowledge_Garden.Controllers
             return File(attachment.blobValue, attachment.ContentType);
         }
 
-        // GET: Attachments/Delete/5
         [Route("Attachments/Delete/{flowerId}/{attachmentName}")]
-        public ActionResult Delete(int flowerId, string attachmentName)
+        public ActionResult Delete(int? flowerId, string attachmentName)
         {
+            if (flowerId == null || attachmentName == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Missing parameters for the request.");
+            }
+
             // Check that attachment exists
             Attachment attachment = uow.Attachments.Find(flowerId, attachmentName);
             if (attachment == null)
@@ -45,13 +129,13 @@ namespace Knowledge_Garden.Controllers
 
             if (!IsOwnedAttachment(attachment))
             {
-                return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+                return new HttpStatusCodeResult(HttpStatusCode.Forbidden, "You do not own the file you request to delete");
             }
-
+            
             // Build view model
             AttachmentViewModel attachmentViewModel = new AttachmentViewModel
             {
-                FlowerId = flowerId,
+                FlowerId = flowerId.Value,
                 Name = attachmentName
             };
             return View(attachmentViewModel);
@@ -59,11 +143,15 @@ namespace Knowledge_Garden.Controllers
 
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(AttachmentViewModel attachmentVM)
+        public ActionResult DeleteConfirmed(int flowerId, string attachmentName)
         {
-            Attachment attachment = uow.Attachments.Find(attachmentVM.FlowerId, attachmentVM.Name);
+            Attachment attachment = uow.Attachments.Find(flowerId, attachmentName);
+            if (!IsOwnedAttachment(attachment))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Forbidden, "You do not own the file you request to delete");
+            }
             uow.Attachments.Remove(attachment);
-            return RedirectToAction("Details", "Flowers", attachmentVM.FlowerId);
+            return RedirectToAction("Details", "Flowers", new { id = flowerId });
         }
 
         protected override void Dispose(bool disposing)
